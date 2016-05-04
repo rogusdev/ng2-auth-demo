7568ad8e4dad862e685e4901d26b87eea39722a2
Chris Rogus <rogusdev@gmail.com>
2016-05-03 22:55:10 -0400

    integrate the aws cognito login as part of the overall login flow with some rxjs observable learning

--- app/user.service.ts
+++ app/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from 'angular2/core';
-import { Http, Headers, Request } from 'angular2/http';
-import {Observable}     from 'rxjs/Observable';
+import { Http, Headers, Request, Response } from 'angular2/http';
+import { Observable }     from 'rxjs/Observable';
 
 // https://www.thepolyglotdeveloper.com/2016/01/include-external-javascript-libraries-in-an-angular-2-typescript-project/
 declare var AWS: any;
@@ -11,65 +11,95 @@ export class UserService {
   private loggedIn = false;
   private identityId = null;
   private devToken = null;
+  private cognitoCredentials = null;
 
   constructor(private http: Http) {
     //this.loggedIn = !!localStorage.getItem('auth_token');
   }
 
-  login(email, password) {
+  login(email: string, password: string): Observable<boolean> {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
 
     // https://angular.io/docs/js/latest/api/http/Http-class.html
     //  using Request object so that I can pass it into aws4 signer, if I choose to go that route (vs api gw genned sdk)
     return this.http.request(new Request({
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify({ email, password }),
-      url: 'https://APIID.execute-api.us-east-1.amazonaws.com/dev/auth/email/login',
-    }))
-      .map(res => {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({ email, password }),
+        url: 'https://APIID.execute-api.us-east-1.amazonaws.com/dev/auth/email/login',
+      }))
+      .map((res: Response) => {
         console.log("RESPONSE");
         console.log(res);
         let body = res.json();
         console.log("BODY");
         console.log(body);
+
+        // "this" changes meaning inside the AWS CognitoIdentity handler,
+        //  so we need a reference to update loggedIn on the current "this"
+        let setCognitoCredentials = cognitoResponse => {
+          if (!cognitoResponse || !cognitoResponse.Credentials)
+          {
+            this.cognitoCredentials = null;
+            this.loggedIn = false;
+          }
+          else
+          {
+            //localStorage.setItem('auth_token', res.auth_token);
+            this.cognitoCredentials = cognitoResponse.Credentials;
+            this.loggedIn = !!this.cognitoCredentials;
+            console.log(this.cognitoCredentials);
+          }
+          return this.loggedIn;
+        };
+
         if (body.login) {
           this.identityId = body.identityId;
           this.devToken = body.token;
 
-          console.log('CHRIS: cognito START');
-          new AWS.CognitoIdentity({
-            region: 'us-east-1',
-          }).getCredentialsForIdentity({
-            IdentityId: this.identityId,
-            Logins: { 'cognito-identity.amazonaws.com': this.devToken }
-          }, function(err, cognitoResponse) {
-            if (err)
-            {
-              console.log("COGNITO ERROR");
-              console.log(err, err.stack); // an error occurred
-              return false;
-            }
-            else
-            {
-              console.log("COGNITO RESPONSE");
-              console.log(cognitoResponse);
+          // http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#static-method-create
+          // https://www.packtpub.com/books/content/rxjs-observable-promise-users-part-2
+          // http://stackoverflow.com/questions/21667824/trying-to-make-my-own-rxjs-observable
+          // https://afterecho.uk/blog/turning-a-callback-into-an-rx-observable.html
+          // http://reactivex.io/documentation/operators/create.html
+          return Observable.create(observer => {
+            console.log('COGNITO START');
+            console.log(observer);
+            console.log(this);
+            console.log(this.identityId);
+            console.log(this.devToken);
 
-              //localStorage.setItem('auth_token', res.auth_token);
-              this.loggedIn = true;
-
-              console.log('CHRIS: cognito END');
-              return this.loggedIn;
-            }
+            new AWS.CognitoIdentity({
+              region: 'us-east-1',
+            }).getCredentialsForIdentity({
+              IdentityId: this.identityId,
+              Logins: { 'cognito-identity.amazonaws.com': this.devToken }
+            }, function(err, cognitoResponse) {
+              if (err)
+              {
+                setCognitoCredentials(false);
+                console.log("COGNITO ERROR");
+                console.log(err, err.stack); // an error occurred
+                observer.error(err);
+              }
+              else
+              {
+                console.log("COGNITO RESPONSE");
+                console.log(cognitoResponse);
+                observer.next(setCognitoCredentials(cognitoResponse));
+                observer.complete()
+              }
+              console.log('COGNITO END');
+            });
           });
         }
-
-        // FIXME: we actually need to use a promise or something to wait for cognito to declare login successful!
-        this.loggedIn = true;
-        return true;
+        else {
+          return Observable.of(setCognitoCredentials(false));
+        }
       })
-      .catch(err =>
+      .switch()  // this collapses the nested observable to lookup the cognito data
+      .catch((err: any) =>
       {
         console.error('ERROR');
         console.error(err);
@@ -78,6 +108,7 @@ export class UserService {
   }
 
   logout() {
+    console.log('logged out');
     this.identityId = null;
     this.devToken = null;
     //localStorage.removeItem('auth_token');
@@ -85,6 +116,7 @@ export class UserService {
   }
 
   isLoggedIn() {
+    console.log('logged in? ' + this.loggedIn);
     return this.loggedIn;
   }
 }
